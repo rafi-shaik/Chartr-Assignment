fe96a81cc632185d982f18482b51ed607ec528de
Rafi Shaik <rafishaik8208@gmail.com>
2023-05-12 20:55:33 +0000

    initial commit

---
+++ src/data.js
@@ -0,0 +1,169 @@
+import { FcGoogle } from "react-icons/fc";
+import { ImLinkedin } from "react-icons/im";
+
+const fetchedData = {
+  data: [
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724921",
+      name: "Vehadarta",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724922",
+      name: "Vedanta",
+      status: "Inactive",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724923",
+      name: "Vikas",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724924",
+      name: "Prakash",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724925",
+      name: "Ravi",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724926",
+      name: "Lokesh",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724927",
+      name: "Varun",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724928",
+      name: "Varisu",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-267852724929",
+      name: "Sathwik",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+    {
+      id: "81f92e17-c1ba-4bc3-9153-2678527249210",
+      name: "Lowan",
+      status: "Active",
+      adminEmail: "Admin.Email@gmail.com",
+      users: "10,434",
+      domain: "",
+      license: "Licensed",
+      signIn: (
+        <div>
+          <FcGoogle size={20} />
+          <ImLinkedin size={20} />
+        </div>
+      ),
+      details: null,
+    },
+  ],
+};
+
+export default fetchedData;
